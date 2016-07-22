1292b13abac4b14632b62ab6acb9d1b209e4b76d
Darshan Huidrom <dhuidrom@regalix-inc.com>
2016-07-22 17:27:40 +0530

    some changes

---
+++ app/src/main/java/com/kangladevelopers/android/combodemo/utility/DeviceInfo.java
@@ -0,0 +1,24 @@
+package com.kangladevelopers.android.combodemo.utility;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+/**
+ * Created by DARSHAN HUIDROJM on 7/18/2016.
+ */
+public class DeviceInfo {
+
+
+    private static String TAG="DeviceInfo";
+   public static final String OS="android";
+
+    public static  String getMACAddress(){
+        WifiManager manager = (WifiManager) MyApplication.getAppContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = manager.getConnectionInfo();
+        String address = info.getMacAddress();
+        Log.d(TAG,"Mac id: "+address);
+        return address;
+    }
+}
