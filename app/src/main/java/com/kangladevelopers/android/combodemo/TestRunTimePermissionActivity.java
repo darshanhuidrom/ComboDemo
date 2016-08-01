c3ffe0061693449bd5a71217f8dee8dcc40a181a
Darshan Huidrom <dhuidrom@regalix-inc.com>
2016-08-01 16:31:21 +0530

    some changes

---
+++ app/src/main/java/com/kangladevelopers/android/combodemo/TestRunTimePermissionActivity.java
@@ -0,0 +1,33 @@
+package com.kangladevelopers.android.combodemo;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class TestRunTimePermissionActivity extends RuntimePermissionsActivity {
+    private static final int REQUEST_PERMISSIONS = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_run_time_permission);
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >=23){
+            // Do something for lollipop and above versions
+            requestAppPermission(new String[]{Manifest.permission.READ_CONTACTS,Manifest.permission.WRITE_EXTERNAL_STORAGE},R.string.app_name,REQUEST_PERMISSIONS);
+
+        } else{
+        }
+
+    }
+
+    @Override
+    public void onPermissionGranted(int requestCode) {
+        Toast.makeText(this, "Permissions Received.", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButtonClick(View view) {
+        Toast.makeText(this, "onButtonClick", Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/kangladevelopers/android/combodemo/utility/DeviceInfo.java
+++ app/src/main/java/com/kangladevelopers/android/combodemo/utility/DeviceInfo.java
@@ -3,6 +3,7 @@ package com.kangladevelopers.android.combodemo.utility;
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 /**
@@ -11,14 +12,19 @@ import android.util.Log;
 public class DeviceInfo {
 
 
-    private static String TAG="DeviceInfo";
-   public static final String OS="android";
+    private static String TAG = "DeviceInfo";
+    public static final String OS = "android";
 
-    public static  String getMACAddress(){
+    public static String getMACAddress() {
         WifiManager manager = (WifiManager) MyApplication.getAppContext().getSystemService(Context.WIFI_SERVICE);
         WifiInfo info = manager.getConnectionInfo();
         String address = info.getMacAddress();
-        Log.d(TAG,"Mac id: "+address);
+        Log.d(TAG, "Mac id: " + address);
         return address;
+    }
+
+    public static String getIMEI() {
+        TelephonyManager telephonyManager = (TelephonyManager)MyApplication.getAppContext().getSystemService(Context.TELEPHONY_SERVICE);
+        return telephonyManager.getDeviceId();
     }
 }
