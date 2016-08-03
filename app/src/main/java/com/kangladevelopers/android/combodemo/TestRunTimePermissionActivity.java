865d1333339d7f5cfbb2a7328e19b2bad522c7d3
Darshan Huidrom <dhuidrom@regalix-inc.com>
2016-08-03 14:15:06 +0530

    some changes

--- app/src/main/java/com/kangladevelopers/android/combodemo/TestRunTimePermissionActivity.java
+++ app/src/main/java/com/kangladevelopers/android/combodemo/TestRunTimePermissionActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.kangladevelopers.android.combodemo.utility.Utility;
+
 public class TestRunTimePermissionActivity extends RuntimePermissionsActivity {
     private static final int REQUEST_PERMISSIONS = 20;
 
@@ -13,6 +15,7 @@ public class TestRunTimePermissionActivity extends RuntimePermissionsActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_run_time_permission);
         int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        Utility.printHashKey(getApplicationContext());
         if (currentapiVersion >=23){
             // Do something for lollipop and above versions
             requestAppPermission(new String[]{Manifest.permission.READ_CONTACTS,Manifest.permission.WRITE_EXTERNAL_STORAGE},R.string.app_name,REQUEST_PERMISSIONS);
